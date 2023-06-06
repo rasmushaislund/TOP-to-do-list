c201e615cfcadf5448de27949a6fd13a3da156ff
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-06-06 14:47:11 +0200

    Add import of index.html and showPicker()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 // START //
 
 import './style.css';
+import './index.html';
 
-console.log('hello');
+const datePickerImg = document.querySelector('.date-pick-icon');
+const dateInput = document.querySelector('.task-date');
+const deadlinePara = document.querySelector('.deadline-para');
+
+datePickerImg.addEventListener('click', () => {
+  try {
+    dateInput.showPicker();
+  } catch (error) {
+    window.alert(error);
+  }
+});
+
+dateInput.addEventListener('input', () => {
+  let getDeadline = dateInput.value;
+  console.log(getDeadline);
+  deadlinePara.textContent = getDeadline;
+  // Add regEx to convert date to desired format (e.g. 'June 12 2023')
+});
